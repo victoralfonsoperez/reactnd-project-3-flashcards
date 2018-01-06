efd96b63368fa7f859978cb796f19aec694a4446
Victor Perez <vaperezr@unal.edu.co>
2018-01-06 16:15:54 -0500

    chore: adds styled components and babel eslint dependencie

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+const Container = styled.View`
+  flex: 1;
+  background-color: #FFF;
+  align-items: center;
+  justify-content: center;
+`
 
-export default class App extends React.Component {
+class App extends Component {
+  state = {
+    name: 'test',
+  }
+
   render() {
+    const { name } = this.state
+
     return (
-      <View style={styles.container}>
+      <Container>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Text>{name}</Text>
+      </Container>
     )
   }
 }
+
+export default App
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # reactnd-project-3-flashcards
-Udacity React NanoDegree third project
+Udacity React NanoDegree third project.
+
+##Table of Contents
+* Getting started
+* Project Description
+
+###Getting Started
+* clone [this](https://github.com/victoralfonsoperez/reactnd-project-3-flashcards) repository
+* cd reactnd-project-3-flashcards
+* install dependencies with `yarn` 
+* start the project with `yarn start`
+
+###Project Description
+This is a React Native project developed using react-native and redux
